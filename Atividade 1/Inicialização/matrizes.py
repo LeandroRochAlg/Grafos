2460c05e20fc8488f3839977c082f6fc4d1b3c33
Leandro Rocha <leandrochaju@hotmail.com>
2023-04-06 18:51:53 -0300

    Update matrizes.py

--- Atividade 1/Inicialização/matrizes.py
+++ Atividade 1/Inicialização/matrizes.py
@@ -4,3 +4,20 @@ def criarMatrizArquivo(instancia):
     with open('C:/Users/rocha/Documents/Unifei/Algoritmos em Grafos/Atividade 1/Instâncias/' + instancia + '.txt', 'rb') as f:
         matriz = np.genfromtxt(f, dtype="int64") #Leitura do arquivo e armazenamento em uma matriz numpy
     return matriz
+
+def criaListaAdjacencias(matriz1):
+    matriz = np.array(matriz1)
+    linha, coluna = matriz.shape
+    listaAdj = {}
+
+    for i in range(linha):
+        listaAdj[i] = []
+        for j in range(coluna):
+            num = matriz[i][j]
+            while(num>0):   #se houverem mais arestas, o vértice será repetido
+                listaAdj[i].append(j)
+                num -= 1
+
+    #print(listaAdj)
+
+    return listaAdj
