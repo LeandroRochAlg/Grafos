f98ddd6c696d9c33e651ce2e76050ab8bb404683
Leandro Rocha <leandrochaju@hotmail.com>
2023-04-18 16:51:37 -0300

    Update manejo.py

--- Atividade 1/Controle/manejo.py
+++ Atividade 1/Controle/manejo.py
@@ -7,6 +7,17 @@ def insereAresta(matriz, vi, vj):
     print(matriz)
     return matriz
 
+def insereArestaLista(lista, vi, vj):
+    lista[vi].append(vj)
+    lista[vi].sort()
+
+    lista[vj].append(vi)
+    lista[vj].sort()
+
+    print(lista)
+
+    return lista
+
 def insereVertice(matriz):
     tam = len(matriz)
     
@@ -22,12 +33,41 @@ def insereVertice(matriz):
     print(matrizNp)
     return matrizNp
 
+def insereVerticeLista(lista):
+    for i in lista:
+        if i == len(lista)-1:
+            lista[i+1] = []
+            break
+
+    print(lista)
+
+    return lista
+
 def removeAresta(matriz, vi, vj):
     matriz[vi][vj] = 0
     matriz[vj][vi] = 0
 
     print(matriz)
     return matriz
+
+def removeArestaLista(lista, vi, vj):
+    listaVi = lista[vi]
+    listaVj = lista[vj]
+
+    for i in range(len(lista[vi])):
+        if listaVi[i] == vj:
+            lista[vi].pop(i)
+            break
+
+    for j in range(len(lista[vj])):
+        if listaVj[j] == vi:
+            lista[vj].pop(j)
+            break
+
+
+    print(lista)
+
+    return lista
 
 def removeVertice(matriz, v):
     for i in range(len(matriz)):
@@ -39,3 +79,18 @@ def removeVertice(matriz, v):
 
     print(matrizNp)
     return matrizNp
+
+def removeVerticeLista(lista, v):
+    del lista[v]
+
+    for j in lista:
+        listaV = lista[j]
+        for i in range(len(listaV)):
+            if listaV[i] == v:
+                while i<len(listaV) and listaV[i] == v:
+                    lista[j].pop(i)
+                break
+
+    print(lista)
+
+    return lista
