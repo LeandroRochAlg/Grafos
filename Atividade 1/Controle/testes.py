4ee2fe268190879b50fc8500d62ca5a1e78e7cd9
Leandro Rocha <leandrochaju@hotmail.com>
2023-04-18 21:19:17 -0300

    Update testes.py

--- Atividade 1/Controle/testes.py
+++ Atividade 1/Controle/testes.py
@@ -1,6 +1,32 @@
 import numpy as np
 from Inicialização import matrizes as mt
 
+def tipoGrafo(matriz1):
+    flagS = 0
+    flagM = 0
+    flagP = 0
+
+    matriz = np.array(matriz1)
+    linha, coluna = matriz.shape
+
+    for i in range(linha):
+        for j in range(coluna):
+            if matriz[i][j] > 1:    #Multigrafo
+                flagM = 20
+            if i == j and matriz[i][j] > 0:    #Pseudografo
+                flagP = 30
+            if matriz[i][j] != matriz[j][i]:    #Digrafo
+                flagS = 1
+
+    if flagP != 0:
+        result = flagP + flagS
+    else:
+        result = flagM + flagS
+
+    print(result)
+
+    return result
+
 def tipoGrafoLista(listaAdj):
     flagS = 0
     flagM = 0
@@ -22,6 +48,16 @@ def tipoGrafoLista(listaAdj):
         result = flagP + flagS
     else:
         result = flagM + flagS
+
+    print(result)
+
+    return result
+
+def verificaAdjacencia(matriz, vi, vj):
+    if matriz[vi][vj] > 0:
+        result = True
+    else:
+        result = False
 
     print(result)
 
