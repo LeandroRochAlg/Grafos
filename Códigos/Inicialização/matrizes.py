5c3495670916a22f4ee8235ff8b7df4142edcf48
Leandro Rocha <leandrochaju@hotmail.com>
2023-04-20 23:04:47 -0300

    Update matrizes.py

--- Códigos/Inicialização/matrizes.py
+++ Códigos/Inicialização/matrizes.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def criarMatrizArquivo(instancia):
-    with open('C:/Users/rocha/Documents/Unifei/Algoritmos em Grafos/Atividade 1/Instâncias/' + instancia + '.txt', 'rb') as f:
+    with open('Grafos/Arquivos/Instâncias/' + instancia + '.txt', 'rb') as f:
         matriz = np.genfromtxt(f, dtype="int64") #Leitura do arquivo e armazenamento em uma matriz numpy
     return matriz
 
