bda209d38d7f81800b90dd88a5c5768762aca2d4
Leandro Rocha <leandrochaju@hotmail.com>
2023-05-05 00:06:51 -0300

    Update busca.py

--- Códigos/Controle/busca.py
+++ Códigos/Controle/busca.py
@@ -1,8 +1,8 @@
-def BFS(listaAdj, valor):
+def BFS(listaAdj, inicio):
     Q = []
     analisado = []
 
-    Q.append(valor)
+    Q.append(inicio)
 
     while len(Q) != 0:
         v = Q[0]
@@ -18,6 +18,41 @@ def BFS(listaAdj, valor):
     for i in listaAdj:
         if len(listaAdj[i]) == 0:
             analisado.append(i)
+
+    #print(analisado)
+
+    return analisado
+
+def DFSr(listaAdj, inicio):
+    analisado = []
+
+    DFS_recursivo(listaAdj, inicio, analisado)
+
+    print(analisado)
+
+def DFS_recursivo(listaAdj, inicio, analisado):
+    analisado.append(inicio)
+    
+    for adj in listaAdj[inicio]:
+        if adj not in analisado:
+            DFS_recursivo(listaAdj, adj, analisado)
+
+def DFS(listaAdj, inicio):
+    analisado = []
+    cont = -1
+
+    while len(analisado) < len(listaAdj):
+        if inicio not in analisado:
+            analisado.append(inicio)
+        
+        for i in range(len(listaAdj)):
+            if i in listaAdj[inicio] and i not in analisado:
+                inicio = i
+                analisado.append(i)
+                break
+            elif i == len(listaAdj) - 1:
+                cont += 1
+                inicio = cont
 
     #print(analisado)
 
