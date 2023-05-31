f5306267f915b56a3a968bef1806bb6e3d33e2fc
Leandro Rocha <leandrochaju@hotmail.com>
2023-05-30 21:13:37 -0300

    Implementação da visita DFS em busca.py

--- Códigos/Controle/busca.py
+++ Códigos/Controle/busca.py
@@ -65,3 +65,32 @@ def DFSi(listaAdj, inicio):
         cont += 1    #reinicia o contador da pilha
 
     return analisado
+
+def visitaDFS(vertice, listaAdj, cor, tipoAresta, tempoD, tempoT, tempo, classifica):
+    cor[vertice] = 'cinza'
+    tempo[0] += 1
+    tempoD[vertice] = tempo[0]
+
+    for adj in listaAdj[vertice]:
+        if cor[adj] == 'branco':
+            tipoAresta[vertice][adj] = 'Tree'
+            if classifica:
+                print(vertice, adj, 'Tree')
+            visitaDFS(adj, listaAdj, cor, tipoAresta, tempoD, tempoT, tempo, classifica)
+        elif cor[adj] == 'cinza':
+            tipoAresta[vertice][adj] = 'Back'
+            if classifica:
+                print(vertice, adj, 'Back')
+        else:
+            if tempoD[vertice] < tempoD[adj]:
+                tipoAresta[vertice][adj] = 'Forward'
+                if classifica:
+                    print(vertice, adj, 'Forward')
+            else:
+                tipoAresta[vertice][adj] = 'Cross'
+                if classifica:
+                    print(vertice, adj, 'Cross')
+    
+    cor[vertice] = 'preto'
+    tempo[0] += 1
+    tempoT[vertice] = tempo[0]
